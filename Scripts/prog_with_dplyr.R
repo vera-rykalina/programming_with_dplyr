b9e23010f2ab647fa7c4e38159c1de9a2519d6e6
vera-rykalina <verarykalina@gmail.com>
2022-05-29 12:50:46 +0200

    Add starts_with() and ends_with()

--- Scripts/prog_with_dplyr.R
+++ Scripts/prog_with_dplyr.R
@@ -10,15 +10,18 @@ world_bank_data <- readRDS("Data/world_bank_data.rds")
 head(world_bank_data)
 # 1. Hold Your Selected Leaders Accountable ####
 
+# select() ####
 # Choose the country, year, usd_conversion_rate, and gdp_in_billions_of_usd columns in that order from imf_data.
 selected_imf <- imf_data %>% 
   select(country, year, usd_conversion_rate, gdp_in_billions_of_usd)
 
+# filter() ####
 # Subset the rows of selected_imf where country is in countries_vector and assign to subsetted_imf; then, print subsetted_imf to check.
 countries_vector <- c("Austria", "Argentina", "Bangladesh", "Belize", "New Zealand", "Tanzania")
                                         
 subsetted_imf <- selected_imf %>% filter(country %in% countries_vector)
-  
+
+# summarise() ####  
 # Calculate the median gdp_in_billions as median_gdp across the years, grouped by each country. Then calculate the median gdp_in_billions as median_gdp across the years, grouped by each country.
 subsetted_imf %>% 
   mutate(gdp_in_billions = gdp_in_billions_of_usd / 
@@ -26,3 +29,19 @@ subsetted_imf %>%
   group_by(country) %>% 
   summarise(median_gdp=median(gdp_in_billions)) 
 # %>% pivot_wider(names_from=country, values_from = median_gdp)
+
+# The helper functions in the tidyselect package: starts_with() and ends_with() from tidyselect and select()
+
+# starts_with() ####
+# Pick columns related to governmental measurements that begin with "gov".
+imf_data %>% 
+  select(starts_with("gov"))
+
+# That selection isn't very helpful, so pick year, country, and those variables starting with "gov" in that order.
+imf_data %>% 
+  select(year, country, starts_with("gov"))
+
+# ends_with()
+# Return year, country, and those columns in imf_data that conclude with "change".
+imf_data %>% 
+  select(year, country, ends_with("change"))
